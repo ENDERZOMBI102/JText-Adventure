e87ab1d8495a6a34f011c386f346937d2ddde2ba
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-01-23 15:05:57 +0000

    added item class, started the main

---
+++ item.java
@@ -0,0 +1,11 @@
+package items;
+
+class item
+{
+    protected double staticDamage;
+    protected String Name;
+    
+    public item()
+    {}
+    {}
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+package main;
+
+import entities.player;
+
+static class main
+{
+    public static void main(String[] args) {
+                
+    }
+}
